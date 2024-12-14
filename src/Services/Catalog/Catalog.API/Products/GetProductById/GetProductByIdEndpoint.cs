fd22dc074c337980cb6fc00a1f17e983e2c79bee
Das-Tier <vitalijmogola@gmail.com>
2024-12-14 15:58:26 +0200

    FInish Catalog API endpoints

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -1,6 +1,5 @@
 namespace Catalog.API.Products.GetProductById;
 
-//public record GetProductByIdRequest();
 public record GetProductByIdResponse(Product Product);
 
 public class GetProductByIdEndpoint : ICarterModule
