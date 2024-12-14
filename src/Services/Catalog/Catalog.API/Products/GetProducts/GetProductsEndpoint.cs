8c7a411a2c916aa189804bdb2912cad4ebbf42ed
Das-Tier <vitalijmogola@gmail.com>
2024-12-14 13:43:16 +0200

    Update GetProductsEndpoint.cs

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,21 @@
 namespace Catalog.API.Products.GetProducts;
 
-public class GetProductsEndpoint
+public class GetProductsEndpoint : ICarterModule
 {
-    
+    private record GetProductsResponse(IEnumerable<Product> Products);
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+                var response = result.Adapt<GetProductsResponse>();
+                return response;
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Products");
+    }
 }
